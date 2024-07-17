8f44ac97d442253f85595a43247d879244d9ef23
Stefano Salsano <stefano.salsano@uniroma2.it>
2024-07-17 21:32:36 +0200

    wip

--- megatron/core/optimizer/optimizer.py
+++ megatron/core/optimizer/optimizer.py
@@ -421,6 +421,7 @@ class MixedPrecisionOptimizer(MegatronOptimizer):
 
     @torch.no_grad()
     def step(self):
+        print("optimizer.py -> step")
         timers = self.config.timers
 
         found_inf_flag = self.prepare_grads()
@@ -446,7 +447,7 @@ class MixedPrecisionOptimizer(MegatronOptimizer):
         num_zeros_in_grad = self.count_zeros() if self.config.log_num_zeros_in_grad else None
         if timers is not None:
             timers('optimizer-count-zeros').stop()
-
+        print("optimizer.py -> step calliing step_with_ready_grads"
         success = self.step_with_ready_grads()    # stack trace 4
 
         # Successful update.
--- megatron/training/training.py
+++ megatron/training/training.py
@@ -642,6 +642,7 @@ def train_step(forward_step_func, data_iterator,
 
     # Update parameters.
     timers('optimizer', log_level=1).start(barrier=args.barrier_with_L1_time)
+    print("train_step calling optimizer.step")
     update_successful, grad_norm, num_zeros_in_grad = optimizer.step()   # stack trace 2
     timers('optimizer').stop()
 
